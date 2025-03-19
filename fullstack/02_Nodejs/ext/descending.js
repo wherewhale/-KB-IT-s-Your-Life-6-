69d46a63ed9e88ac78686464879aae81be590318
wherewhale <syh980130@gmail.com>
2025-03-19 17:24:55 +0900

    feat(vue): add chapter3 and practices

---
+++ fullstack/02_Nodejs/ext/descending.js
@@ -0,0 +1,4 @@
+const score = [82, 96, 54, 76, 92, 99, 69, 88];
+console.log('before = ' + score);
+score.sort().reverse();
+console.log('after = ' + score);
---
+++ fullstack/02_Nodejs/ext/join.js
@@ -0,0 +1,6 @@
+const ar = [0, 1, 2, 3];
+console.log('ar = ' + ar.join());
+console.log('ar = ' + ar.join(', '));
+console.log('ar = ' + ar.join(' -> '));
+console.log('ar = ' + ar.toString());
+console.log('ar = ' + ar);
